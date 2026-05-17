84650fbf7ca44f11276f15e9230457bf735e19cb
Tim Sippell <tjsippell@gmail.com>
2026-05-16 21:12:40 -0700

    add windows build script

---
+++ build-win.bat
@@ -0,0 +1,12 @@
+@echo off
+setlocal
+
+echo Configuring build...
+cmake -B build_win -S . -DBUILD_SHARED_LIBS=OFF -DSFML_STATIC_LIBRARIES=TRUE
+
+echo Building Release...
+cmake --build build_win --config Release
+
+echo.
+echo Done. Output: build_win\src\Release\faidworks-input-viewer.exe
+echo Remember to include the resources/ folder when distributing.
